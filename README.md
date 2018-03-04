b30acb6e312977c1e7f540a004a7a4c4916547e4
Patryk Poźniak <ppozniak95@gmail.com>
2018-03-04 13:56:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# create-phaser-app
+
+## WIP - This doesn't work yet!
+If you're interested in contributing please visit http://www.html5gamedevs.com/topic/35798-phaser-3-boilerplates-starters-list-discussion-and-features/
---
+++ index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const package = require('./package.json');
+const log = console.log;
+
+const projectName = process.argv[2];
+const currDir = process.cwd();
+
+log(chalk`Running {green ${package.name} ${package.version}}`);
+log();
+
+if(!projectName) {
+  log(chalk`{red You have to specify project name}`);
+  log(chalk`Example usage: {cyan create-phaser-app ping-pong}`);
+  log();
+  process.exit(1);
+}
+
+log();
+log(chalk`Welcome to Phaser app creator.`);
+
+const QUESTIONS = [
+  {
+    name: 'language',
+    type: 'list',
+    message: 'What language are you going to use?',
+    choices: ['JavaScript', 'Typescript', 'CoffeeScript']
+  }
+];
+
+inquirer.prompt(QUESTIONS)
+  .then(answers => {
+    const { language } = answers;
+    const newProjectPath = `${currDir}/${projectName}`;
+    const templatePath = `${__dirname}/templates/${language.toLowerCase()}`;
+
+    log();
+    log(chalk`Creating a new Phaser app in {green ${newProjectPath} } with {cyan ${language}} settings.`);
+
+    fs.mkdirSync(newProjectPath);
+    createDirectoryContents(templatePath, newProjectPath);
+  });
+
+function createDirectoryContents(templatePath, newProjectPath) {
+  const filesToCreate = fs.readdirSync(templatePath);
+
+  filesToCreate.forEach(file => {
+    const origFilePath = `${templatePath}/${file}`;
+    const stats = fs.statSync(origFilePath);
+
+    if (stats.isFile()) {
+      const contents = fs.readFileSync(origFilePath, 'utf8');
+
+      const writePath = `${newProjectPath}/${file}`;
+      fs.writeFileSync(writePath, contents, 'utf8');
+    } else if (stats.isDirectory()) {
+      fs.mkdirSync(`${currDir}/${newProjectPath}/${file}`);
+
+      createDirectoryContents(`${templatePath}/${file}`, `${newProjectPath}/${file}`);
+    }
+  });
+}
