4900f83db3ede61e169b6607b68ac816a5fe82ad
Patryk Poźniak <ppozniak95@gmail.com>
2018-03-11 15:32:59 +0100

    Prepare monorepo

---
+++ packages/create-phaser-app/index.js
@@ -0,0 +1,7 @@
+// const createPhaser = require('./lib');
+// const messages = require('./lib/messages');
+
+// module.exports = {
+//   messages: messages,
+//   createPhaserApp: createPhaser,
+// };
